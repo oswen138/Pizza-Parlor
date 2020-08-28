ad4cdd0eea34d02c01fd3bcda71193c2b6091881
ophelia swen <ophelia.swen@gmail.com>
2020-08-28 13:40:11 -0700

    add prototype method to call on topping objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
+//Pizza Constructor
+
 function Pizza() {
   this.toppings
-  this.sauce =
+  this.size =
   this.price = 
   this.toppingsPrice = 0;
   this.grandTotal = 0;
 }
 
-Pizza.prototpy.add Size = function(size) {
-  this.size
+
+//prototype method to call on topping objects
+
+function Topping(mushroom, salami, bacon) {
+  this.mushroomTopping = mushroomTopping;
+  this.salamiTopping = salamiTopping;
+  this.baconTopping = baconTopping;
 }
+
+//prototype method to call on topping objects
+
+let tomatoes = { name: "Tomatoes", price: 2.99 };
+let cucumbers = { name: "Cucumbers", price: 0.99 };
+let onions = { name: "Onions", price: 0.79 };
+
+stores.forEach(function(store) {
+  console.log(store.name + " sells:");
+  store.products.forEach(function(product) {
+    console.log(product.name);
+  });
+
+
