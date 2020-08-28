c2067c8e1191c71ffe5d8635d7daf3c0e69a295a
ophelia swen <ophelia.swen@gmail.com>
2020-08-28 15:49:47 -0700

    clean up commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //Business Logic for PizzaOrder ---------
 
 function PizzaOrder() {
-  this.grandTotal = [],
-  this.sauce = [];
+  this.grandTotal = grandTotal,
+  this.sauce = 0
   this.toppings = 0
   this.size = 0
   this.saucePrice = 0
@@ -43,149 +43,13 @@ PizzaOrder.prototype.addTopping = function(topping) {
   this.countTotal();
 }
 
-//Push selections into grandTotal array
-PizzaOrder.prototype.addgrandTotal = function(sauce, topping) {
-  this.grandTotal.push();
+//Calculate total cost of pizza
+PizzaOrder.prototype.countTotal = function() {
+  this.grandTotal = this.saucePrice + this.toppingPrice;
+  this.showTotal();
 }
 
-
-stores.forEach(function(store) {
-  console.log(store.name + " sells:");
-  store.products.forEach(function(product) {
-    console.log(product.name);
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  this.sauce = 0
-  this.toppings = 0
-  this.size = 0
-}
-
-pizzaSquare.prototype.addToppings = function(topping) {
-  topping.price = this.assignPrice();
-  this.toppings.push(topping);
-}
-pizzaSquare.prototype.assignPrice = function() {
-  this.toppingsPrice +=1;
-  return this.toppingsPrice;
-}
-
-
-//Business Logic for Toppings 
-function Toppings() {
-  this.toppings = [];
-  this.toppingsPrice = 0;
-}
-pizzaSquare.prototype.addToppings = function(topping) {
-  topping.price = this.assignPrice();
-  this.toppings.push(topping);
-}
-pizzaSquare.prototype.assignPrice = function() {
-  this.toppingsPrice +=1;
-  return this.toppingsPrice;
-}
-//Business Logic for Pizza
-function Pizza() {
-  this.grandTotal = grandTotal;
-}
-
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
-
-
-
-pizzaSquare.prototype.findTopping = function(price) {
-  for (let i=0; i< this.toppings.length; i++) {
-      if (this.toppings[i].price == price) {
-        return this.toppings[i];
-      }
-    };
-  return false;
-}
-
-
-function Topping(mushroom, salami, bacon) {
-  this.mushroomTopping = mushroomTopping;
-  this.salamiTopping = salamiTopping;
-  this.baconTopping = baconTopping;
-}
-
-//prototype method to call on topping objects
-
-let tomatoes = { name: "Tomatoes", price: 2.99 };
-let cucumbers = { name: "Cucumbers", price: 0.99 };
-let onions = { name: "Onions", price: 0.79 };
-
-stores.forEach(function(store) {
-  console.log(store.name + " sells:");
-  store.products.forEach(function(product) {
-    console.log(product.name);
-  });
-
-
-  function AddressBook() {
-    this.contacts = [];
-  }
-  
-  function Contact(firstName, lastName, phoneNumber) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phoneNumber = phoneNumber;
-  }
-  
-  Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName;
-  }
-
-  //Business Logic for Pizza
-  
-  Pizza.prototype.addToppings = function(topping) {
-    this.toppings.push(topping);
-  }
-
-// User Interface Logic ---------
-let pizzaSquare = new PizzaSquare();
-
-function displaytoppingDetails(toppingsToDisplay) {
-}
-
-//does tomsh
-function attachPizzaListeners() {
-$("ul#pizzaSquare").on("click", "li", function() {
-  console.log("The id of this <li> is " + this.id + ".");
-});
-};
-
-//showOrder(orderId) {
-  const order = pizzaSquare.findOrder(orderID);
-  $('#show-order").show();
-  $(".pizza-size").html(order.pizzaSize);
-  $(".pizza-sauce").html(order.pizzaSauce);
-  $(".pizza-topping").html(order.pizzaTopping);
-  let buttons = $("#buttons");
-  buttons.empty();
-  buttons.append("<button class='deleteButton' id=" + order.id+">Delete</button>");
-}
+//UI Logic for PizzaOrder ------------
 
 $(document).ready(function() {
   attachContactListeners();  
@@ -206,20 +70,23 @@ $(document).ready(function() {
   });
 });
 
-const beverage = $("#beverage").val();
-
-function displayCurrentBalance(currentBalance) {
-  $("#balance").text(currentBalance);
-}
 
 
-$("form#pizza-order").submit(function(event) {
-  event.preventDefault();
 
-  const sauce = parseFloat($("#sauce").val());
-  const topping = parseFloat($("#topping").val());
 
-  currentOrder = order(currentOrder);
-  displayCurrentOrder(currentOrder);
-});
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
